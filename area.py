6a34987cb7ed5443c76a7c4dbcc04a13c6fc7349
marlisar <marlisa@rebaum.com>
2022-07-30 14:12:51 -0700

    first commit

---
+++ area.py
@@ -0,0 +1,11 @@
+def houseSize(length, width):
+    area = length * width
+    return area
+
+
+from math import pi
+
+def circleCircum(diameter):
+    circumference = pi*diameter
+    return circumference
+
